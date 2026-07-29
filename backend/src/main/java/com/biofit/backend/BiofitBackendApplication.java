3651e5c5cccba21e393e66b529787f8fc235ea26
ramh75279 <ramh75379@gmail.com>
2026-07-29 19:22:11 +0530

    project structure

---
+++ backend/src/main/java/com/biofit/backend/BiofitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.biofit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiofitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiofitBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/biofit/backend/BiofitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biofit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiofitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
